414fbe18a9c49f05ffba65c2eafc7a9fd174fc0a
David Vos <vosdavid2@gmail.com>
2024-11-12 21:06:54 +0100

    3.0 The vec3 Class

---
+++ src/vec3.rs
@@ -0,0 +1,138 @@
+use std::fmt::{Display, Formatter};
+use std::ops::{Add, AddAssign, Div, DivAssign, Index, IndexMut, Mul, MulAssign, Neg, Sub};
+
+#[derive(Default)]
+pub struct Vec3 {
+    e: [f64; 3],
+}
+
+impl Vec3 {
+    pub fn new(e0: f64, e1: f64, e2: f64) -> Self {
+        Self { e: [e0, e1, e2] }
+    }
+
+    pub fn x(&self) -> f64 { self.e[0] }
+    pub fn y(&self) -> f64 { self.e[1] }
+    pub fn z(&self) -> f64 { self.e[2] }
+
+    pub fn length(&self) -> f64 {
+        self.length_squared().sqrt()
+    }
+
+    pub fn length_squared(&self) -> f64 {
+        self.x() * self.x() + self.y() * self.y() + self.z() * self.z()
+    }
+    
+    pub fn dot(&self, rhs: &Self) -> f64 {
+        self.x() * rhs.x() + self.y() * rhs.y() + self.z() * rhs.z()
+    }
+    
+    pub fn cross(self, rhs: &Self) -> Self {
+        Self::new(
+            self.y() * rhs.z() - self.z() * rhs.y(),
+            self.z() * rhs.x() - self.x() * rhs.z(),
+            self.x() * rhs.y() - self.y() * rhs.x(),
+        )
+    }
+}
+
+impl Neg for Vec3 {
+    type Output = Self;
+
+    fn neg(self) -> Self::Output {
+        Vec3 { e: [-self.e[0], -self.e[1], -self.e[2]] }
+    }
+}
+
+impl Index<u32> for Vec3 {
+    type Output = f64;
+
+    fn index(&self, index: u32) -> &Self::Output {
+        &self.e[index as usize]
+    }
+}
+
+impl IndexMut<u32> for Vec3 {
+    fn index_mut(&mut self, index: u32) -> &mut Self::Output {
+        &mut self.e[index as usize]
+    }
+}
+
+impl AddAssign for Vec3 {
+    fn add_assign(&mut self, v: Self) {
+        self.e[0] += v.e[0];
+        self.e[1] += v.e[1];
+        self.e[2] += v.e[2];
+    }
+}
+
+impl MulAssign<f64> for Vec3 {
+    fn mul_assign(&mut self, t: f64) {
+        self.e[0] *= t;
+        self.e[1] *= t;
+        self.e[2] *= t;
+    }
+}
+
+impl DivAssign<f64> for Vec3 {
+    fn div_assign(&mut self, t: f64) {
+        *self *= 1.0 / t;
+    }
+}
+
+impl Display for Vec3 {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{} {} {}", self.e[0], self.e[1], self.e[2])
+    }
+}
+
+impl Add for &Vec3 {
+    type Output = Vec3;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Vec3::new(self.x() + rhs.x(), self.y() + rhs.y(), self.z() + rhs.z())
+    }
+}
+
+impl Sub for &Vec3 {
+    type Output = Vec3;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        Vec3::new(self.x() - rhs.x(), self.y() - rhs.y(), self.z() - rhs.z())
+    }
+}
+
+impl Mul for &Vec3 {
+    type Output = Vec3;
+
+    fn mul(self, rhs: Self) -> Self::Output {
+        Vec3::new(self.x() * rhs.x(), self.y() * rhs.y(), self.z() * rhs.z())
+    }
+}
+
+impl Mul<f64> for &Vec3 {
+    type Output = Vec3;
+
+    fn mul(self, t: f64) -> Self::Output {
+        Vec3::new(self.x() * t, self.y() * t, self.z() * t)
+    }
+}
+
+impl Mul<&Vec3> for f64 {
+    type Output = Vec3;
+
+    fn mul(self, vec: &Vec3) -> Self::Output {
+        vec * self
+    }
+}
+
+impl Div<&Vec3> for f64 {
+    type Output = Vec3;
+
+    fn div(self, vec: &Vec3) -> Self::Output {
+        (1.0 / self) * vec
+    }
+}
+
+// Just an alias, but useful for clarity
+pub type Point3 = Vec3;
