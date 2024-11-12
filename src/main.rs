11195d400145bb40e38beeb5493fdca540e3d696
David Vos <vosdavid2@gmail.com>
2024-11-12 21:59:31 +0100

    4.2 Sending Rays Into the Scene

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,60 @@
-use crate::color::Color;
+use crate::color::{color_to_string, Color};
+use crate::ray::Ray;
+use crate::vec3::{Point3, Vec3};
 
 mod vec3;
 mod color;
 mod ray;
 
 fn main() {
-    // Image
-    let image_width = 256;
-    let image_height = 256;
+    // Image setup
+    let aspect_ratio = 16.0 / 9.0;
+    let image_width = 400;
+    let image_height = (image_width as f64 / aspect_ratio) as u32;
+    let image_height = if image_height < 1 { 1 } else { image_height };
+
+    // Camera setup
+    let focal_length = 1.0;
+    let viewport_height = 2.0;
+    let viewport_width = viewport_height * (image_width as f64 / image_height as f64);
+    let camera_center = Point3::default();
+
+    // Calculate vectors across viewport edges
+    let viewport_u = Vec3::new(viewport_width, 0.0, 0.0);
+    let viewport_v = Vec3::new(0.0, -viewport_height, 0.0);
+
+    // Calculate delta vectors from pixel to pixel
+    let pixel_delta_u = viewport_u / image_width as f64;
+    let pixel_delta_v = viewport_v / image_height as f64;
+
+    // Calculate location of upper left pixel
+    let viewport_upper_left = camera_center
+        - Vec3::new(0.0, 0.0, focal_length)
+        - (viewport_u / 2.0)
+        - (viewport_v / 2.0);
+    let pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
 
     let mut ppm_string = format!("P3\n{image_width} {image_height}\n255\n");
 
     for j in 0..image_height {
-        eprintln!("Scanlines remaining: {}", image_height -j);
+        eprintln!("Scanlines remaining: {}", image_height - j);
         for i in 0..image_width {
-            let pixel_color = Color::new(
-                i as f64 / (image_width - 1) as f64,
-                j as f64 / (image_height - 1) as f64,
-                0.0
-            );
-            ppm_string += &pixel_color.to_string();
+            let pixel_center = pixel00_loc +
+                (i as f64 * pixel_delta_u) + (j as f64 * pixel_delta_v);
+            let ray_direction = pixel_center - camera_center;
+            let r = Ray::new(camera_center, ray_direction);
+            
+            let pixel_color = ray_color(&r);
+            ppm_string += color_to_string(&pixel_color).as_str();
         }
     }
 
     eprintln!("Done.");
     println!("{ppm_string}");
+}
+
+fn ray_color(r: &Ray) -> Color {
+    let unit_direction = r.direction().unit();
+    let a = 0.5 * (unit_direction.y() + 1.0);
+    (1.0 - a) * Color::new(1.0, 1.0, 1.0) + a * Color::new(0.5, 0.7, 1.0)
 }
