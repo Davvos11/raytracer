d481fce46032ae069e27703f82be19374eda1f22
Davvos11 <vosdavid2@gmail.com>
2024-11-13 00:14:22 +0100

    8.1 Some Random Number Utilities

--- src/rtweekend.rs
+++ src/rtweekend.rs
@@ -1,3 +1,14 @@
+use rand::Rng;
+
 pub fn degrees_to_radians(degrees: f64) -> f64 {
     degrees * std::f64::consts::PI / 180.0
+}
+
+pub fn random_double() -> f64 {
+    let mut rng = rand::thread_rng();
+    rng.gen()
+}
+
+pub fn random_double_range(min: f64, max: f64) -> f64 {
+    min + (max - min) * random_double()
 }
