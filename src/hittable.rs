dca079d27d430b4b9618ac99dfd3eec6eb15b2b7
David Vos <vosdavid2@gmail.com>
2024-11-12 22:41:11 +0100

    6.4 Front Faces Versus Back Faces

--- src/hittable.rs
+++ src/hittable.rs
@@ -6,6 +6,16 @@ pub struct HitRecord {
     pub p: Point3,
     pub normal: Vec3,
     pub t: f64,
+    pub front_face: bool,
+}
+
+impl HitRecord {
+    /// Sets the hit record normal vector.
+    /// Note: the parameter `outward_normal` is assumed to have unit length.
+    pub fn set_face_normal(&mut self, r: &Ray, outward_normal: Vec3) {
+        self.front_face = r.direction().dot(&outward_normal) < 0.0;
+        self.normal = if self.front_face { outward_normal } else { -outward_normal };
+    }
 }
 
 pub trait Hittable {
