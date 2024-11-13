fb62863b212734b843958d15d13d37cbbf6070b4
Davvos11 <vosdavid2@gmail.com>
2024-11-13 01:03:12 +0100

    9.5 Using Gamma Correction for Accurate Color Intensity

--- src/color.rs
+++ src/color.rs
@@ -7,6 +7,10 @@ pub fn color_to_string(pixel_color: &Color) -> String {
     let r = pixel_color.x();
     let g = pixel_color.y();
     let b = pixel_color.z();
+    
+    let r = linear_to_gamma(r);
+    let g = linear_to_gamma(g);
+    let b = linear_to_gamma(b);
 
     // Translate the [0,1] RGB values to the byte range [0,255]
     let intensity = Interval::new(0.0, 0.999);
@@ -15,4 +19,12 @@ pub fn color_to_string(pixel_color: &Color) -> String {
     let bbyte = (256.0 * intensity.clamp(b)) as u32;
 
     format!("{rbyte} {gbyte} {bbyte}\n")
+}
+
+fn linear_to_gamma(linear_component: f64) -> f64 {
+    if linear_component > 0.0 {
+        linear_component.sqrt()
+    } else {
+        0.0
+    }
 }
