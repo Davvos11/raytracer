54688bd26028734a8d708d81b8ee3e4f4485e4ae
Davvos11 <vosdavid2@gmail.com>
2024-11-13 16:42:58 +0100

    10.6 Fuzzy Reflection

--- src/material.rs
+++ src/material.rs
@@ -34,19 +34,22 @@ impl Material for Lambertian {
 
 pub struct Metal {
     albedo: Color,
+    fuzz: f64,
 }
 
 impl Metal {
-    pub fn new(albedo: Color) -> Self {
-        Self { albedo }
+    pub fn new(albedo: Color, fuzz: f64) -> Self {
+        Self { albedo, fuzz }
     }
 }
 
 impl Material for Metal {
     fn scatter(&self, r_in: &Ray, rec: &HitRecord, attenuation: &mut Color, scattered: &mut Ray) -> bool {
         let reflected = Vec3::reflect(r_in.direction(), &rec.normal);
+        let reflected = reflected.unit() + (self.fuzz * Vec3::random_unit());
         *scattered = Ray::new(rec.p, reflected);
         *attenuation = self.albedo;
-        true
+        
+        scattered.direction().dot(&rec.normal) > 0.0
     }
 }
