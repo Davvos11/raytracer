7a0e5a2d72127764b46adbf8ee16066dc3aa7d9a
David Vos <vosdavid2@gmail.com>
2024-11-13 15:44:55 +0100

    10.3 Modeling Light Scatter and Reflectance

--- src/material.rs
+++ src/material.rs
@@ -1,7 +1,27 @@
 use crate::color::Color;
 use crate::hittable::HitRecord;
 use crate::ray::Ray;
+use crate::vec3::Vec3;
 
 pub trait Material {
     fn scatter(&self, r_in: &Ray, hit_record: &HitRecord, attenuation: &mut Color, scattered: &mut Ray) -> bool;
+}
+
+pub struct Lambertian {
+    albedo: Color,
+}
+
+impl Material for Lambertian {
+    fn scatter(&self, _r_in: &Ray, rec: &HitRecord, attenuation: &mut Color, scattered: &mut Ray) -> bool {
+        let mut scatter_direction = rec.normal + Vec3::random_unit();
+
+        // Catch degenerate scatter direction
+        if scatter_direction.near_zero() {
+            scatter_direction = rec.normal;
+        }
+
+        *scattered = Ray::new(rec.p, scatter_direction);
+        *attenuation = self.albedo;
+        true
+    }
 }
--- src/vec3.rs
+++ src/vec3.rs
@@ -59,6 +59,11 @@ impl Vec3 {
     pub fn length_squared(&self) -> f64 {
         self.x() * self.x() + self.y() * self.y() + self.z() * self.z()
     }
+    
+    pub fn near_zero(&self) -> bool {
+        let s = 1e-8;
+        self.x().abs() < s && self.y().abs() < s && self.z().abs() < s
+    }
 
     pub fn dot(&self, rhs: &Self) -> f64 {
         self.x() * rhs.x() + self.y() * rhs.y() + self.z() * rhs.z()
