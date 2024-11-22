1029d6edf9223e904b345bc901f32545aa06aace
David Vos <vosdavid2@gmail.com>
2024-11-22 14:27:55 +0100

    add cli options for scene files

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use crate::material::{Dielectric, Lambertian, Material, Metal};
 use crate::sphere::Sphere;
 use crate::vec3::{Point3, Vec3};
 use std::rc::Rc;
+use clap::Parser;
 use crate::hittable::Hittable;
 use crate::rtweekend::{random_double, random_double_range};
 
@@ -20,60 +21,37 @@ mod interval;
 mod camera;
 mod material;
 
+#[derive(Parser)]
+struct Cli {
+    /// The input / output file for the scene / world
+    filename: String,
+    /// Whether to generate a scene file
+    #[arg(long, short, default_value_t = false)]
+    write: bool,
+}
+
 fn main() {
-    // World setup
-    let mut world = HittableList::default();
+    // Parse CLI arguments
+    let args = Cli::parse();
 
-    let ground_material = Rc::new(Lambertian::new(Color::new(0.5, 0.5, 0.5)));
-    world.add(Rc::new(Sphere::new(Point3::new(0.0, -1000.0, 0.0), 1000.0, ground_material)));
+    let world = if args.write {
+        let mut world = HittableList::default();
+        let ground_material = Rc::new(Lambertian::new(Color::new(0.5, 0.5, 0.5)));
+        world.add(Rc::new(Sphere::new(Point3::new(0.0, -1000.0, 0.0), 1000.0, ground_material)));
+        // Specify more elements of the scene here
 
-    for a in -11..11 {
-        for b in -11..11 {
-            let choose_mat = random_double();
-            let center = Point3::new(
-                a as f64 + 0.9 * random_double(),
-                0.2,
-                b as f64 * 0.9 + random_double(),
-            );
+        // Serialize the world
+        let file = File::create(args.filename).expect("Could not open file");
+        serde_json::to_writer(&file, &world).expect("Could not write to file");
 
-            if (center - Point3::new(4.0, 0.2, 0.0)).length() > 0.9 {
-                if choose_mat < 0.8 {
-                    // Diffuse
-                    let albedo = Color::random() * Color::random();
-                    let material = Rc::new(Lambertian::new(albedo));
-                    world.add(Rc::new(Sphere::new(center, 0.2, material)));
-                } else if choose_mat < 0.95 {
-                    // Metal
-                    let albedo = Color::random_range(0.5, 1.0);
-                    let fuzz = random_double_range(0.0, 0.5);
-                    let material = Rc::new(Metal::new(albedo, fuzz));
-                    world.add(Rc::new(Sphere::new(center, 0.2, material)));
-                } else {
-                    // Glass
-                    let material = Rc::new(Dielectric::new(1.5));
-                    world.add(Rc::new(Sphere::new(center, 0.2, material)));
-                }
-            }
-        }
-    }
+        world
+    } else {
+        // Deserialize the object
+        let file = File::open(args.filename).expect("Could not open file");
+        serde_json::from_reader(&file).expect("Could not read file")
+    };
 
-    let material_1 = Rc::new(Dielectric::new(1.5));
-    world.add(Rc::new(Sphere::new(Point3::new(0.0, 1.0, 0.0), 1.0, material_1)));
 
-    let material_2 = Rc::new(Lambertian::new(Color::new(0.4, 0.2, 0.1)));
-    world.add(Rc::new(Sphere::new(Point3::new(-4.0, 1.0, 0.0), 1.0, material_2)));
-
-    let material_3 = Rc::new(Metal::new(Color::new(0.7, 0.6, 0.5), 0.0));
-    world.add(Rc::new(Sphere::new(Point3::new(4.0, 1.0, 0.0), 1.0, material_3)));
-
-    // Serialize the world
-    let file = File::create("world.json").expect("Could not open world.json");
-    serde_json::to_writer(&file, &world).expect("Could not write to world.json");
-
-    // Deserialize the object
-    let file = File::open("world.json").expect("Could not open world.json");
-    let world: HittableList = serde_json::from_reader(&file).expect("Could not read world.json");
-    
     let mut cam = Camera::new();
     cam.aspect_ratio = 16.0 / 9.0;
     cam.image_width = 1200;
